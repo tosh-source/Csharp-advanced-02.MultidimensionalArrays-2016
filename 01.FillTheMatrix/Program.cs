2988c9dcb99668772bdb4e612e16b1a93b97fb8f
user <user@DESKTOP-TSSHSEQ>
2017-04-20 23:23:57 +0300

    1. Some changes in task "FillTheMatrix"; 2. Add new task "06.MatrixClass";

--- 01.FillTheMatrix/Program.cs
+++ 01.FillTheMatrix/Program.cs
@@ -15,49 +15,37 @@ namespace _01.FillTheMatrix
             char matrixType = char.Parse(Console.ReadLine());
             int[,] matrix = new int[matrixSize, matrixSize];
             int counter = 1;
-            //matrix type 'a', calculation
-            if (matrixType == 'a')
-            {
-                for (int col = 0; col <= matrixSize - 1; col++)
-                {
-                    for (int row = 0; row <= matrixSize - 1; row++, counter++)
-                    {
-                        matrix[row, col] = counter;
-                    }
-                }
-            }
-            else if (matrixType == 'b') //matrix type 'b', calculation
-            {
-                for (int col = 0; col <= matrixSize - 1; col++)
-                {
-                    for (int row = 0; row <= matrixSize - 1; row++, counter++)
-                    {
-                        if ((col % 2) == 0)
-                        {
-                            matrix[row, col] = counter;
-                        }
-                        else
-                        {
-                            matrix[row, col] = counter + (matrixSize - (row * 2) - 1);  //"counter + (matrixSize - (row * 2) - 1)" is equal to ==> counter-- (started from DOUBLED last one counter++)
-                        }
-                    }
 
-                }
-            }
-            else if (matrixType == 'c') //matrix type "c" (diagonal matrix), calculation 
+            //calculation
+            switch (matrixType)
             {
-                DiagonalMatrix(matrix, matrixSize, counter);
+                case 'a':  //matrix type 'a'
+                    MatrixTypeA(matrix, matrixSize, counter);
+                    break;
+
+                case 'b':  //matrix type 'b'
+                    MatrixTypeB(matrix, matrixSize, counter);
+                    break;
+
+                case 'c':  //matrix type "c" (diagonal matrix) 
+                    DiagonalMatrix(matrix, matrixSize, counter);
+                    break;
+
+                case 'd':  //matrix type 'd' (spiral matrix)
+                    SpiralMatrix(matrix, matrixSize, counter);
+                    break;
             }
-            else if (matrixType == 'd') //matrix type 'd' (spiral matrix), calculation
-            {
-                SpiralMatrixCalc(matrix, matrixSize, counter);
-            }
+
             //print
+            PrintMatrix(matrixSize, matrix);
+        }
+
+        private static void PrintMatrix(int matrixSize, int[,] matrix)
+        {
             for (int row = 0; row <= matrixSize - 1; row++)
             {
                 for (int col = 0; col <= matrixSize - 1; col++)
                 {
-                    
                     if (col == (matrixSize - 1)) //last element without spacing 
                     {
                         Console.Write(matrix[row, col]);
@@ -71,6 +59,35 @@ namespace _01.FillTheMatrix
             }
         }
 
+        private static void MatrixTypeA(int[,] matrix, int matrixSize, int counter)
+        {
+            for (int col = 0; col <= matrixSize - 1; col++)
+            {
+                for (int row = 0; row <= matrixSize - 1; row++, counter++)
+                {
+                    matrix[row, col] = counter;
+                }
+            }
+        }
+
+        private static void MatrixTypeB(int[,] matrix, int matrixSize, int counter)
+        {
+            for (int col = 0; col <= matrixSize - 1; col++)
+            {
+                for (int row = 0; row <= matrixSize - 1; row++, counter++)
+                {
+                    if ((col % 2) == 0)
+                    {
+                        matrix[row, col] = counter;
+                    }
+                    else
+                    {
+                        matrix[row, col] = counter + (matrixSize - (row * 2) - 1);  //"counter + (matrixSize - (row * 2) - 1)" is equal to ==> counter-- (started from DOUBLED last one counter++)
+                    }
+                }
+            }
+        }
+
         static void DiagonalMatrix(int[,] matrix, int matrixSize, int counter)
         {
             //first half of diagonal matrix
@@ -78,14 +95,7 @@ namespace _01.FillTheMatrix
             {
                 for (int col = 0; col < (matrixSize - reversedRow); col++, counter++)
                 {
-                    if (col == 0)
-                    {
-                        matrix[reversedRow, col] = counter;
-                    }
-                    else
-                    {
-                        matrix[(reversedRow + col), col] = counter;
-                    }
+                    matrix[(reversedRow + col), col] = counter;
                 }
             }
             //second half of diagonal matrix
@@ -93,19 +103,12 @@ namespace _01.FillTheMatrix
             {
                 for (int row = 0; row < (matrixSize - col); row++, counter++)
                 {
-                    if (row == 0)
-                    {
-                        matrix[row, col] = counter;
-                    }
-                    else
-                    {
-                        matrix[row, (col + row)] = counter;
-                    }
+                    matrix[row, (col + row)] = counter;
                 }
             }
         }
 
-        static void SpiralMatrixCalc(int[,] matrix, int matrixSize, int counter)
+        static void SpiralMatrix(int[,] matrix, int matrixSize, int counter)
         {
             for (int depth = 0; depth <= (matrixSize / 2); depth++)
             {
